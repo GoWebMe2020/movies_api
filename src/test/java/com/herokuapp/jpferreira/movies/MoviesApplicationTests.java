35f944fbd475b1cafc13969126523935b45033ba
JP Ferreira <jpferreira1982@gmail.com>
2023-03-31 13:56:44 +1100

    Initial commit of spring boot project

---
+++ src/test/java/com/herokuapp/jpferreira/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuapp.jpferreira.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
