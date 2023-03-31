79289a2f95cf9581a27d6cba02508da9d3235d9b
JP Ferreira <jpferreira1982@gmail.com>
2023-03-31 16:28:17 +1100

    Adding functionality to return one movie by imdbId

--- src/main/java/com/herokuapp/jpferreira/movies/MovieService.java
+++ src/main/java/com/herokuapp/jpferreira/movies/MovieService.java
@@ -2,7 +2,9 @@ package com.herokuapp.jpferreira.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -10,5 +12,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> movie(String imdbId) {
+        return movieRepository.findByImdbId(imdbId);
     }
 }
